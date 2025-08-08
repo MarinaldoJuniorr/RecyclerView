3dfb4231b2c3beb2b9ceb4bb50547c419f1d0bf0
Marinaldo  JR <marinaldosfs17@gmail.com>
2025-08-08 23:58:36 +0100

    Building a contact list contain recycler view, mocking data and changing from the list view to a grid list.

---
+++ app/src/main/java/com/devspace/recyclerview/ContactListMock.kt
@@ -0,0 +1,83 @@
+package com.devspace.recyclerview
+
+val contacts = listOf(
+    Contact(
+        icon = R.drawable.sample1,
+        name = "Juliana Souza",
+        phone = "+55 (11) 91234-5678"
+    ),
+    Contact(
+        icon = R.drawable.sample2,
+        name = "Rafael Almeida",
+        phone = "+55 (11) 92345-6789"
+    ),
+    Contact(
+        icon = R.drawable.sample3,
+        name = "Diego Soares",
+        phone = "+55 (11) 93456-7890"
+    ),
+    Contact(
+        icon = R.drawable.sample4,
+        name = "Paulo Dantas",
+        phone = "+55 (11) 94567-8901"
+    ) , Contact(
+        icon = R.drawable.sample5,
+        name = "Amanda Velkof",
+        phone = "(11) 95678-9012"
+    ),
+    Contact(
+        icon = R.drawable.sample6,
+        name = "Alanna Paiva",
+        phone = "(11) 96789-0123"
+    ),
+    Contact(
+        icon = R.drawable.sample7,
+        name = "Ingrid Carpes",
+        phone = "(11) 97890-1234"
+    ),
+    Contact(
+        icon = R.drawable.sample8,
+        name = "Naldo Junior",
+        phone = "(11) 98901-2345"
+    ),
+    Contact(
+        icon = R.drawable.sample9,
+        name = "Tiquinho Soares",
+        phone = "(11) 99012-3456"
+    ),
+    Contact(
+        icon = R.drawable.sample10,
+        name = "Diogo Pituca",
+        phone = "(11) 90123-4567"
+    ),
+    Contact(
+        icon = R.drawable.sample11,
+        name = "Cristiano Ronaldo",
+        phone = "(11) 91357-2468"
+    ),
+    Contact(
+        icon = R.drawable.sample12,
+        name = "Rodrigo Pinto",
+        phone = "(11) 92468-1357"
+    ),
+    Contact(
+        icon = R.drawable.sample13,
+        name = "Camila Volpon",
+        phone = "(11) 99753-0864"
+    ),
+    Contact(
+        icon = R.drawable.sample14,
+        name = "Maria Marques",
+        phone = "(11) 90246-1234"
+    ),
+    Contact(
+        icon = R.drawable.sample15,
+        name = "Carolina Silva",
+        phone = "(11) 91357-9246"
+    ),
+    Contact(
+        icon = R.drawable.sample15,
+        name = "Pedro Ribeiro",
+        phone = "(11) 93794-9285"
+    )
+    )
